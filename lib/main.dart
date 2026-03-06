dfbede4f19dfdb898a00cfbd3881b56743f10529
Roger Agostinho <167251142+rogeragostinho@users.noreply.github.com>
2026-03-06 16:59:50 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:looply/db/db_initializer.dart';
 import 'package:looply/repository/revision_cycle_repository.dart';
 import 'package:looply/repository/tag_repository.dart';
 import 'package:looply/repository/topic_repository.dart';
 import 'package:looply/ui/app/main_app.dart';
 
-void main() async{
-  WidgetsFlutterBinding.ensureInitialized();
+void main(){
+  //WidgetsFlutterBinding.ensureInitialized();
 
-  final topic = TopicRepository();
-  await topic.init();
-
-  final revision = RevisionCycleRepository();
-  await revision.init();
-
-  final tag = TagRepository();
-  await tag.init();
+  DbInitializer.init();
 
   runApp(const MainApp());
 }
