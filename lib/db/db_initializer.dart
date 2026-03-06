5d32596e6a455d6739b68c9eb7428c6b810d3878
Roger Agostinho <167251142+rogeragostinho@users.noreply.github.com>
2026-03-06 15:20:27 +0100

    refactor: cria uma classe para a criação das tabelas

---
+++ lib/db/db_initializer.dart
@@ -0,0 +1,51 @@
+// db_tables_creator.dart
+import 'package:sqflite/sqflite.dart';
+import 'database_con.dart';
+import '../core/db_constants.dart';
+
+class DbInitializer {
+  DbInitializer._(); // Construtor privado, não instancia
+
+  /// Inicializa todas as tabelas
+  static Future<void> init() async {
+    final db = await DatabaseCon.instance;
+
+    await _createTopicsTable(db);
+    await _createRevisionCycleTable(db);
+    await _createTagsTable(db);
+  }
+
+  static Future<void> _createTopicsTable(Database db) async {
+    await db.execute('''
+      CREATE TABLE IF NOT EXISTS ${DBTables.topics} (
+        ${TopicsColumns.colId} INTEGER PRIMARY KEY AUTOINCREMENT,
+        ${TopicsColumns.colName} TEXT,
+        ${TopicsColumns.colRevisionCycle} TEXT,
+        ${TopicsColumns.colTags} TEXT,
+        ${TopicsColumns.colNote} TEXT,
+        ${TopicsColumns.colRevisions} TEXT,
+        ${TopicsColumns.colImages} TEXT,
+        ${TopicsColumns.colStudiedOn} TEXT
+      )
+    ''');
+  }
+
+  static Future<void> _createRevisionCycleTable(Database db) async {
+    await db.execute('''
+      CREATE TABLE IF NOT EXISTS ${DBTables.revisionCycle} (
+        ${RevisionCycleColumns.colId} INTEGER PRIMARY KEY AUTOINCREMENT,
+        ${RevisionCycleColumns.colName} TEXT,
+        ${RevisionCycleColumns.colCycle} TEXT
+      )
+    ''');
+  }
+
+  static Future<void> _createTagsTable(Database db) async {
+    await db.execute('''
+      CREATE TABLE IF NOT EXISTS ${DBTables.tags} (
+        ${TagsColumns.colId} INTEGER PRIMARY KEY AUTOINCREMENT,
+        ${TagsColumns.colName} TEXT
+      )
+    ''');
+  }
+}
