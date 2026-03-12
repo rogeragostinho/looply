85c3f6e6d354c940c9361f6fa97e3651a029c061
Roger Agostinho <167251142+rogeragostinho@users.noreply.github.com>
2026-03-12 18:06:46 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:looply/db/db_initializer.dart';
-import 'package:looply/ui/app/main_app.dart';
+import 'package:looply/my_app.dart';
 
 void main(){
   WidgetsFlutterBinding.ensureInitialized();
 
   DbInitializer.init();
 
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
