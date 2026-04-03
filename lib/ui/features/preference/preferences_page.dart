549d65c60f8ecea9f4f5004490f5b1d841de6f62
Roger Agostinho <167251142+rogeragostinho@users.noreply.github.com>
2026-04-03 21:54:15 +0100

    style: improve preferences page UI

--- lib/ui/features/preference/preferences_page.dart
+++ lib/ui/features/preference/preferences_page.dart
@@ -4,19 +4,34 @@ import 'package:looply/router/app_routes.dart';
 import 'package:looply/ui/core/widgets/app_top_bar.dart';
 
 class PreferencesPage extends StatelessWidget {
-
   const PreferencesPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppTopBar(title: "Preferências"),
-      body: Column(
+      body: ListView(
         children: [
-          Text("Preferences"),
-          ElevatedButton(onPressed: () {
-            context.push(AppRoutes.tags);
-          }, child: Text("Tags")),
+          SizedBox(height: 20),
+          ListTile(
+            leading: Icon(Icons.tag),
+            title: Text("Tags"),
+            trailing: Icon(Icons.chevron_right),
+            onTap: () => context.push(AppRoutes.tags),
+          ),
+          Divider(
+            thickness: 1, // espessura da linha
+            height: 20, // espaço vertical total ocupado (linha incluída)
+            indent: 16, // espaço à esquerda antes da linha começar
+            endIndent: 16, // espaço à direita onde a linha termina
+            color: const Color.fromARGB(68, 158, 158, 158), // cor da linha
+          ),
+          ListTile(
+            leading: Icon(Icons.tag),
+            title: Text("Tags"),
+            trailing: Icon(Icons.chevron_right),
+            onTap: () => context.push(AppRoutes.tags),
+          ),
         ],
       ),
     );
